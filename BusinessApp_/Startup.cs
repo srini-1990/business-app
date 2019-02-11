cfbb73be0073dfc757f718523e30aa89d9475637
srini-1990 <srinivas.boddupalli@saven.in>
2019-02-11 21:53:57 +0530

    First Commit

---
+++ BusinessApp_/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BusinessApp_.Startup))]
+namespace BusinessApp_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
